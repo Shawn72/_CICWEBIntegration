0c36b93e6ead21d6c77e2c6f0924202ebb91aa6e
Shawn Andre Mbuvi <shawnmbuvi@gmail.com>
2019-02-01 11:49:13 +0300

    Add project files.

---
+++ _CICWEBIntegration/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace _CICWEBIntegration.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ _CICWEBIntegration/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace _CICWEBIntegration.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
